536865873dcf7f05f31512d63d569605e99a8d69
maty0505git <maty0505@gmail.com>
2023-10-27 11:34:02 +0900

    Initial commit

---
+++ src/components/pages/UserManagement.tsx
@@ -0,0 +1,17 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, VFC } from "react";
+import { UserCard } from "../organisms/user/UserCard";
+
+export const UserManagement: VFC = memo(() => {
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="じゃけぇ"
+          fullName="Takumi Okada"
+        />
+      </WrapItem>
+    </Wrap>
+  );
+});
